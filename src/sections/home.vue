6b6bbf455fd100ed83cb8ed8830080ddc456d3b9
Tanisha <tanisha031199@gmail.com>
2019-11-19 00:19:43 +0530

    added

--- src/sections/home.vue
+++ src/sections/home.vue
@@ -1,17 +1,15 @@
 <template>
   <section class="home-page">
     <h1>HomePage</h1>
+    <button>Shop</button>
   </section>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
-  components: {
-  }
-}
+  name: "Home",
+  components: {}
+};
 </script>
 
-<style lang="css">
-</style>
+<style lang="css"></style>
