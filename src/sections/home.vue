dc88a2867ad2b2051e87bfacff74813041088b2c
Tanisha <tanisha031199@gmail.com>
2019-11-22 21:00:04 +0530

    added

--- src/sections/home.vue
+++ src/sections/home.vue
@@ -1,7 +1,12 @@
 <template>
   <section class="home-page">
-    <h1>HomePage</h1>
-    <button @click="$router.push('shop')">Shop</button>
+    <div class="circle">
+      <span class="title">A VUE SHOP</span
+      ><button class="button" @click="$router.push('shop')">Shop</button>
+    </div>
+    <div class="text">
+      Life is hard enough already. Let us make it a little easier.
+    </div>
   </section>
 </template>
 
@@ -12,4 +17,61 @@ export default {
 };
 </script>
 
-<style lang="css"></style>
+<style lang="css">
+.home-page {
+  background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9zYLbP5lDBSylTkAjG7Csu85EzUIyLArvww4cJ9v-ASWfqw9c&s);
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: 100% 100%;
+  background-blend-mode: lighten;
+  background-color: rgba(255, 255, 255, 0.3);
+  height: 800px;
+}
+.circle {
+  width: 300px;
+  height: 300px;
+  border: 2px solid white;
+  border-radius: 50%;
+  display: inline-block;
+  /* top: 200px; */
+  position: relative;
+  animation: 2s ease-out 0s 1 forwards bottomtotop;
+}
+.title {
+  position: relative;
+  top: 100px;
+  font-size: 30px;
+  font-weight: 900;
+  left: 50px;
+  color: white;
+}
+.button {
+  position: relative;
+  top: 150px;
+  right: 80px;
+}
+.text {
+  position: relative;
+  top: 250px;
+  font-size: 30px;
+  left: -2000px;
+  color: white;
+  animation: 1.5s ease-out 2s 1 forwards lefttoright;
+}
+@keyframes bottomtotop {
+  0% {
+    top: 2000px;
+  }
+  100% {
+    top: 200px;
+  }
+}
+@keyframes lefttoright {
+  0% {
+    left: -2000px;
+  }
+  100% {
+    left: 0px;
+  }
+}
+</style>
